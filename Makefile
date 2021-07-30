ea756ffa1f04abb23633781407430c90100dae9a
A1rM1ke <garen.arzumanyan@gmail.com>
2021-07-30 23:03:03 +0400

    Initial Commit
    The first commit where everything is setup.

---
+++ Makefile
@@ -0,0 +1,2 @@
+compile:
+	g++-11 -Wall -o program.out main.cpp
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <nlohmann/json.hpp>
+
+using json = nlohmann::json;
+
+int main() {
+
+    json j;
+
+    return 0;
+}
